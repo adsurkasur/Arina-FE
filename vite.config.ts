eb5d1050d33858b396234c6d81589a3d9d1e8ace
adsurkasur <superyusuf55@gmail.com>
2025-05-17 01:32:28 +0700

    100000000 times reinstall

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import tailwindcss from '@tailwindcss/vite';
+// This file is not needed and can cause Vite config conflicts. Please use vite.config.mjs only.
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-});
+// export default defineConfig({
+//   plugins: [react()],
+  // resolve: {
+  //   alias: {
+  //     '@': path.resolve(__dirname, 'src'),
+  //   },
+  // },
+// });
